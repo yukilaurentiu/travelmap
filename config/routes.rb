bbabcd3bd3e467991f87786740adcfd27f905b55
yuki <yuki.laurentiu@gmail.com>
2023-11-17 14:51:06 +0100

    added contact page

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   resources :attendances, only: :destroy
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get "about", to: "pages#about"
   # Defines the root path route ("/")
   # root "articles#index"
 end
