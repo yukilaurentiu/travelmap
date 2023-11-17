c38a917cef8dfcfcb60669e0e26f10eefe90e369
yuki <yuki.laurentiu@gmail.com>
2023-11-17 17:09:50 +0100

    changed position to space-around

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   resources :attendances, only: :destroy
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  get "about", to: "pages#about"
+  get "contact", to: "pages#contact"
   # Defines the root path route ("/")
   # root "articles#index"
 end
